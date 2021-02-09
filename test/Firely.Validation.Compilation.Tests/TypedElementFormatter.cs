9988c9d9b667ba2e3f91bb665209689ee5cf7a47
Ewout Kramer <e.kramer@furore.com>
2021-02-09 19:52:24 +0100

    Made serialization of ITypedElement also use property names.

--- test/Firely.Validation.Compilation.Tests/TypedElementFormatter.cs
+++ test/Firely.Validation.Compilation.Tests/TypedElementFormatter.cs
@@ -49,16 +49,37 @@ namespace MessagePack.Formatters
             var stringFormatter = options.Resolver.GetFormatterWithVerify<string>();
             var valueFormatter = TypedElementValueFormatter.Instance;
 
+#if MSGPACK_KEY
             if (reader.ReadArrayHeader() != 5) throw new InvalidOperationException("Incorrect binary format: expected exactly 5 elements in serialized ITypedElement.");
+#else
+            if (reader.ReadMapHeader() != 5) throw new InvalidOperationException("Incorrect binary format: expected exactly 5 elements in serialized ITypedElement.");
+#endif
+
+#if !MSGPACK_KEY
+            _ = reader.ReadString();  // Name
+#endif 
 
             var name = stringFormatter.Deserialize(ref reader, options);
-            var result = new SimpleTypedElement(name)
-            {
-                InstanceType = stringFormatter.Deserialize(ref reader, options),
-                Value = valueFormatter.Deserialize(ref reader, options),
-                Location = stringFormatter.Deserialize(ref reader, options)
-            };
+            var result = new SimpleTypedElement(name);
 
+#if !MSGPACK_KEY
+            _ = reader.ReadString();  // InstanceType
+#endif
+            result.InstanceType = stringFormatter.Deserialize(ref reader, options);
+
+#if !MSGPACK_KEY
+            _ = reader.ReadString();  // Value
+#endif
+            result.Value = valueFormatter.Deserialize(ref reader, options);
+
+#if !MSGPACK_KEY
+            _ = reader.ReadString();  // Location
+#endif
+            result.Location = stringFormatter.Deserialize(ref reader, options);
+
+#if !MSGPACK_KEY
+            _ = reader.ReadString();  // Children
+#endif
             var childCount = reader.ReadArrayHeader();
             options.Security.DepthStep(ref reader);
 
@@ -93,12 +114,29 @@ namespace MessagePack.Formatters
             var stringFormatter = options.Resolver.GetFormatterWithVerify<string>();
             var valueFormatter = TypedElementValueFormatter.Instance;
 
-            // The ITypedElement is serialized as an array of 5 items: Name, InstanceType, etc.
+#if MSGPACK_KEY
+            // The ITypedElement is serialized as an array of 5 items: Name, InstanceType, etc.         
             writer.WriteArrayHeader(5);
             stringFormatter.Serialize(ref writer, value!.Name, options);
             stringFormatter.Serialize(ref writer, value.InstanceType, options);
             valueFormatter.Serialize(ref writer, value.Value, options);
             stringFormatter.Serialize(ref writer, value.Location, options);
+#else
+            writer.WriteMapHeader(5);
+            writer.Write("Name");
+            stringFormatter.Serialize(ref writer, value!.Name, options);
+
+            writer.Write("InstanceType");
+            stringFormatter.Serialize(ref writer, value.InstanceType, options);
+
+            writer.Write("Value");
+            valueFormatter.Serialize(ref writer, value.Value, options);
+
+            writer.Write("Location");
+            stringFormatter.Serialize(ref writer, value.Location, options);
+
+            writer.Write("Children");
+#endif
 
             var children = value.Children().ToList();
             writer.WriteArrayHeader(children.Count);
