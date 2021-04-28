9ef7758002ba02d9c42c9550e07eda186b50caf6
Ewout Kramer <e.kramer@furore.com>
2021-04-28 10:13:36 +0200

    Introduced Canonical type

---
+++ src/Firely.Fhir.Validation/Support/Canonical.cs
@@ -0,0 +1,89 @@
+﻿/* 
+ * Copyright (C) 2021, Firely (info@fire.ly) - All Rights Reserved
+ * Proprietary and confidential. Unauthorized copying of this file, 
+ * via any medium is strictly prohibited.
+ */
+
+using System;
+
+namespace Firely.Fhir.Validation
+{
+    /// <summary>
+    /// Represents a FHIR canonical uri, which is an uri plus an optional version indicator.
+    /// </summary>
+    public record Canonical
+    {
+        public Canonical(string original)
+        {
+            Original = original ?? throw new ArgumentNullException(nameof(original));
+
+            (Uri, Version) = splitCanonical(original);
+        }
+
+        public void Deconstruct(out string uri, out string? version)
+        {
+            uri = Uri;
+            version = Version;
+        }
+
+        public string Original { get; private set; }
+
+        /// <summary>
+        /// The uri part of the canonical, which is the canonical without the version indication.
+        /// </summary>
+        public string Uri { get; private set; }
+
+        /// <summary>
+        /// The version string of the canonical (if present).
+        /// </summary>
+        public string? Version { get; private set; }
+
+        /// <summary>
+        /// Whether the canonical is a relative or an absolute uri.
+        /// </summary>
+        public bool IsAbsolute => ToUri().IsAbsoluteUri;
+
+        /// <summary>
+        /// Whether the canonical has a version part.
+        /// </summary>
+        public bool HasVersion => Version is not null;
+
+        /// <summary>
+        /// Converts the canonical back to the full canonical as passed to the
+        /// constructor.
+        /// </summary>
+        /// <remarks>Returns the <see cref="Original" /> property.</remarks>
+        public override string ToString() => Original;
+
+        /// <summary>
+        /// Converts the canonical to a <see cref="System.Uri" />.
+        /// </summary>
+        /// <returns></returns>
+        public Uri ToUri() => new(Original, UriKind.RelativeOrAbsolute);
+
+        /// <summary>
+        /// Converts a string to a canonical.
+        /// </summary>
+        public static implicit operator Canonical(string s) => new(s);
+
+        /// <summary>
+        /// Converts a canonical to a string.
+        /// </summary>
+        /// <remarks>Returns the <see cref="Original" /> property.</remarks>
+        public static explicit operator string(Canonical c) => c.Original;
+
+        /// <summary>
+        /// Splits a canonical into its url and its version string.
+        /// </summary>
+        private static (string url, string? version) splitCanonical(string canonical)
+        {
+            if (canonical.EndsWith('|')) canonical = canonical[..^1];
+
+            var position = canonical.LastIndexOf('|');
+
+            return position == -1 ?
+                (canonical, null)
+                : (canonical[0..position], canonical[(position + 1)..]);
+        }
+    }
+}
---
+++ test/Firely.Fhir.Validation.Tests/Impl/CanonicalTests.cs
@@ -0,0 +1,95 @@
+﻿/* 
+ * Copyright (C) 2021, Firely (info@fire.ly) - All Rights Reserved
+ * Proprietary and confidential. Unauthorized copying of this file, 
+ * via any medium is strictly prohibited.
+ */
+
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Firely.Fhir.Validation.Tests
+{
+    [TestClass]
+    public class CanonicalTests
+    {
+        [TestMethod]
+        public void TestConstruction()
+        {
+            var testee = new Canonical("http://example.org/test");
+            testee.Original.Should().Be("http://example.org/test");
+            testee.Uri.Should().Be("http://example.org/test");
+            testee.HasVersion.Should().BeFalse();
+            testee.Version.Should().BeNull();
+            testee.IsAbsolute.Should().BeTrue();
+
+            testee = new Canonical("http://example.org/test|3.4.5");
+            testee.Original.Should().Be("http://example.org/test|3.4.5");
+            testee.Uri.Should().Be("http://example.org/test");
+            testee.HasVersion.Should().BeTrue();
+            testee.Version.Should().Be("3.4.5");
+            testee.IsAbsolute.Should().BeTrue();
+
+            testee = new Canonical("urn:a:b:c");
+            testee.Original.Should().Be("urn:a:b:c");
+            testee.Uri.Should().Be("urn:a:b:c");
+            testee.HasVersion.Should().BeFalse();
+            testee.Version.Should().BeNull();
+            testee.IsAbsolute.Should().BeTrue();
+
+            testee = new Canonical("local");
+            testee.Original.Should().Be("local");
+            testee.Uri.Should().Be("local");
+            testee.HasVersion.Should().Be(false);
+            testee.Version.Should().BeNull();
+            testee.IsAbsolute.Should().BeFalse();
+
+            testee = new Canonical("#anchor");
+            testee.Original.Should().Be("#anchor");
+            testee.Uri.Should().Be("#anchor");
+            testee.HasVersion.Should().Be(false);
+            testee.Version.Should().BeNull();
+            testee.IsAbsolute.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void TestDeconstruction()
+        {
+            var (uri, version) = new Canonical("http://example.org/test|3.4.5");
+            uri.Should().Be("http://example.org/test");
+            version.Should().Be("3.4.5");
+
+            (uri, version) = new Canonical("http://example.org/test");
+            uri.Should().Be("http://example.org/test");
+            version.Should().BeNull();
+        }
+
+        [TestMethod]
+        public void TestConversion()
+        {
+            var testee = (Canonical)"http://example.org/test|3.4.5";
+            testee.Original.Should().Be("http://example.org/test|3.4.5");
+
+            var asstring = (string)testee;
+            asstring.Should().Be("http://example.org/test|3.4.5");
+
+            asstring = testee.ToString();
+            asstring.Should().Be("http://example.org/test|3.4.5");
+
+            var uri = testee.ToUri();
+            uri.OriginalString.Should().Be("http://example.org/test|3.4.5");
+        }
+
+        [TestMethod]
+        public void TestEquivalence()
+        {
+            var t1 = new Canonical("http://example.org/test|3.4.5");
+            var t2 = new Canonical("http://example.org/test|3.4.5");
+            var t3 = new Canonical("http://example.org/test");
+
+            (t1 == t2).Should().BeTrue();
+            t1.Equals(t2).Should().BeTrue();
+            (t1 == t3).Should().BeFalse();
+            t1.Equals(t3).Should().BeFalse();
+        }
+    }
+}
