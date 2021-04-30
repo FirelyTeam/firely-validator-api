7acee91ed138d9c62dcadb669cfd47a887f495a4
Ewout Kramer <e.kramer@furore.com>
2021-04-30 15:50:19 +0200

    Improved detection of cycles by updating ReferenceState only for the child schema of the ReferencedInstanceValidator.

--- src/Firely.Fhir.Validation/Schema/ReferenceState.cs
+++ src/Firely.Fhir.Validation/Schema/ReferenceState.cs
@@ -4,26 +4,34 @@
  * via any medium is strictly prohibited.
  */
 
-using System.Collections.Concurrent;
+using System;
 using System.Linq;
 
 namespace Firely.Fhir.Validation
 {
     internal class ReferenceState
     {
+        private static readonly string[] EMPTY = Array.Empty<string>();
         /// <summary>
-        /// All references that the validator visisted
+        /// All references that the validator visited
         /// </summary>
-        private readonly ConcurrentBag<ReferenceId> _referencesVisited = new();
+        private string[] _referencesVisited;
 
-        public void AddReference(string location, string reference, string? targetProfile = null)
+        public ReferenceState WithNewReference(string reference)
         {
-            _referencesVisited.Add(new(location, reference, targetProfile));
+            var newReferencesVisited = new string[_referencesVisited.Length + 1];
+            _referencesVisited.CopyTo(newReferencesVisited, 0);
+            newReferencesVisited[_referencesVisited.Length] = reference;
+
+            return new ReferenceState
+            {
+                _referencesVisited = newReferencesVisited
+            };
+
         }
 
-        public bool Visited(string location, string reference, string? targetProfile = null)
-            => _referencesVisited.Contains(new(location, reference, targetProfile));
+        public bool Visited(string reference) => _referencesVisited.Contains(reference);
 
-        private record ReferenceId(string Location, string Reference, string? TargetProfile = null);
+        public ReferenceState() => _referencesVisited = EMPTY;
     }
 }
