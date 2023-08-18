c2d622e76900c2d249b1704c1853f15ed0c2ae9c
Marten Smits <m.smits@furore.com>
2023-08-18 12:04:44 +0200

    Clean up code after PR review

--- src/Firely.Fhir.Validation.Compilation.Shared/EnterpriseSchemaBuilders/ElementDefinitionBuilder.cs
+++ src/Firely.Fhir.Validation.Compilation.Shared/EnterpriseSchemaBuilders/ElementDefinitionBuilder.cs
@@ -11,8 +11,6 @@ namespace Firely.Fhir.Validation.Compilation
         /// <inheritdoc/>
         public IEnumerable<IAssertion> Build(ElementDefinitionNavigator nav, ElementConversionMode? conversionMode = ElementConversionMode.Full)
         {
-            if (conversionMode == ElementConversionMode.ContentReference) yield break;
-
             if (nav.Current.Path == "ElementDefinition")
             {
                 yield return new ElementDefinitionValidator();
--- src/Firely.Fhir.Validation/EnterpriseValidators/ElementDefinitionValidator.cs
+++ src/Firely.Fhir.Validation/EnterpriseValidators/ElementDefinitionValidator.cs
@@ -34,84 +34,53 @@ namespace Firely.Fhir.Validation
         /// <param name="input"></param>
         /// <param name="state"></param>
         /// <returns></returns>
-        private static List<ResultReport> validateTypeCompatibilityOfValues(ITypedElement input, ValidationState state)
+        private static IEnumerable<ResultReport> validateTypeCompatibilityOfValues(ITypedElement input, ValidationState state)
         {
             var typeNames = getTypeNames(input);
-            var evidence = new List<ResultReport>();
+
 
             if (typeNames.Any())
             {
-                var fixedType = input.Children("fixed").FirstOrDefault()?.InstanceType;
-                var patternType = input.Children("pattern").FirstOrDefault()?.InstanceType;
-                var exampleTypes = input.Children("example")?
-                                        .Select(e => e.Children("value")
-                                            .FirstOrDefault()?.InstanceType)
-                                        .Where(e => e is not null);
+                var fixedType = input.Children("fixed").Select(f => f.InstanceType);
+                var patternType = input.Children("pattern").Select(f => f.InstanceType);
+                var exampleTypes = input.Children("example")
+                                        .SelectMany(e => e.Children("value")
+                                            .Select(c => c.InstanceType));
+                var minValueType = input.Children("minValue").Select(m => m.InstanceType);
+                var maxValueType = input.Children("maxValue").Select(m => m.InstanceType);
 
-                var minValueType = input.Children("minValue").FirstOrDefault()?.InstanceType;
-                var maxValueType = input.Children("maxValue").FirstOrDefault()?.InstanceType;
+                var fixedEvidence = validateType(fixedType, "fixed", typeNames, input, state);
+                var patternEvidence = validateType(patternType, "pattern", typeNames, input, state);
+                var examplesEvidence = validateType(exampleTypes, "example.value", typeNames, input, state);
+                var minValueEvidence = validateType(minValueType, "minValue", typeNames, input, state);
+                var maxValueEvidence = validateType(maxValueType, "maxValue", typeNames, input, state);
 
-                if (fixedType is not null)
-                {
-                    validateType(fixedType, "fixed", typeNames, evidence, input, state);
-                }
-
-                if (patternType is not null)
-                {
-                    validateType(patternType, "pattern", typeNames, evidence, input, state);
-                }
-
-                if (exampleTypes?.Any() == true)
-                {
-                    foreach (var exampleType in exampleTypes)
-                    {
-                        validateType(exampleType!, "example.value", typeNames, evidence, input, state);
-                    }
-                }
-
-                if (minValueType is not null)
-                {
-                    validateType(minValueType, "minValue", typeNames, evidence, input, state);
-                }
-
-                if (maxValueType is not null)
-                {
-                    validateType(maxValueType, "maxValue", typeNames, evidence, input, state);
-                }
+                return fixedEvidence
+                    .Concat(patternEvidence)
+                    .Concat(examplesEvidence)
+                    .Concat(minValueEvidence)
+                    .Concat(maxValueEvidence);
             }
 
-
-            return evidence;
+            return Enumerable.Empty<ResultReport>();
         }
 
         private static IEnumerable<string> getTypeNames(ITypedElement input)
         {
-            var typesComponents = input.Children("type");
-            if (typesComponents is not null && typesComponents.Any())
-            {
-                var typeNames = typesComponents.Select(t => t.Children("code")
-                                .FirstOrDefault()?
-                                .Value?.ToString());
-
-                //remove null values.
-                if (typeNames != null && typeNames.Any())
-                {
-                    return typeNames!
-                           .Where(n => n is not null)
-                           .Select(n => n!);
-                }
-            }
-
-            return Enumerable.Empty<string>();
+            var typeComponents = input.Children("type");
+            return typeComponents.SelectMany(t =>
+                                        t.Children("code")
+                                        .TakeWhile(c => c.Value?.ToString() is not null))
+                                        .Select(c => c.Value.ToString());
         }
 
-        private static void validateType(string valueType, string propertyName, IEnumerable<string> typeNames, List<ResultReport> evidence, ITypedElement input, ValidationState state)
+        private static IEnumerable<ResultReport> validateType(IEnumerable<string> valueTypes, string propertyName, IEnumerable<string> typeNames, ITypedElement input, ValidationState state)
         {
-            if (!typeNames.Contains(valueType))
-            {
-                var issue = new IssueAssertion(Issue.PROFILE_ELEMENTDEF_INCORRECT, $"Type of the {propertyName} property '{valueType}' doesn't match with the type(s) of the element '{string.Join(',', typeNames)}'");
-                evidence.Add(issue.AsResult(input, state));
-            }
+            return valueTypes.Where(t => !typeNames.Contains(t))
+                                      .Select(t => new IssueAssertion(Issue.PROFILE_ELEMENTDEF_INCORRECT, $"Type of the {propertyName} property '{t}' doesn't match with the type(s) of the element '{string.Join(',', typeNames)}'").AsResult(input, state));
+
         }
     }
 }
+
+#nullable restore
