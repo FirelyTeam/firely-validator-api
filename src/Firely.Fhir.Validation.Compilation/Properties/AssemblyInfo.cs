1b8f0c05807fda9e2bc662a4aaa60c3f71e9617a
Marco Visser <marco@fire.ly>
2023-11-20 16:15:16 +0100

    fine tuning devops and extra room for adding new builders

--- src/Firely.Fhir.Validation.Compilation/Properties/AssemblyInfo.cs
+++ src/Firely.Fhir.Validation.Compilation/Properties/AssemblyInfo.cs
@@ -1,13 +1,11 @@
 ﻿/* 
- * Copyright (C) 2021, Firely (info@fire.ly) - All Rights Reserved
+ * Copyright (C) 2023, Firely (info@fire.ly) - All Rights Reserved
  * Proprietary and confidential. Unauthorized copying of this file, 
  * via any medium is strictly prohibited.
  */
 
 using System;
 using System.Runtime.CompilerServices;
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: CLSCompliant(true)]
 
