719771f335fd7909142e4be5b8d760028ea5ce21
Kasdejong <k.j2.de.jong@student.vu.nl>
2025-04-29 12:27:04 +0200

    PR requested changes

--- test/Firely.Fhir.Validation.Compilation.Tests.Shared/ExtensionContextComponentTests.cs
+++ test/Firely.Fhir.Validation.Compilation.Tests.Shared/ExtensionContextComponentTests.cs
@@ -9,7 +9,7 @@ public class ExtensionContextComponentTests : IClassFixture<SchemaBuilderFixture
 
     public ExtensionContextComponentTests(SchemaBuilderFixture fixture) => _fixture = fixture;
 
-    [Fact(Skip = "Temporarily disabled")]
+    [Fact(Skip = "Temporarily disabled because extension context is not yet supported in this version of the validator")]
     public void CreatesExtensionContextSchema()
     {
         var schema = _fixture.SchemaResolver.GetSchema(TestProfileArtifactSource.CONTEXTCONSTRAINEDEXTENSION);
