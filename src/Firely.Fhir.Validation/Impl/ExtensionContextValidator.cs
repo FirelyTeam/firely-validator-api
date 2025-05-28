82e18481a8b3f2ff3dabac5cce12fc19075bbbfb
Kasdejong <k.j2.de.jong@student.vu.nl>
2025-05-28 15:17:28 +0200

    This introduced incorrect behaviour, reverting part of the changes...

--- src/Firely.Fhir.Validation/Impl/ExtensionContextValidator.cs
+++ src/Firely.Fhir.Validation/Impl/ExtensionContextValidator.cs
@@ -2,7 +2,6 @@ using Hl7.Fhir.ElementModel;
 using Hl7.Fhir.Introspection;
 using Hl7.Fhir.Model;
 using Hl7.Fhir.Support;
-using Hl7.Fhir.Utility;
 using Hl7.FhirPath;
 using Newtonsoft.Json.Linq;
 using System;
@@ -140,7 +139,9 @@ public class ExtensionContextValidator : IValidatable
         }
         
         if(current == null) return false;
-        var modelInspector = ((ITypedElement)instance).Annotation<ModelInspector>() ?? ModelInspector.Base;
+#pragma warning disable CS0618 // Type or member is obsolete
+        var modelInspector = ModelInspector.ForType(current.Poco.GetType());
+#pragma warning restore CS0618 // Type or member is obsolete
         return modelInspector.IsInstanceTypeFor(type, current.Poco.TypeName);
     }
 
