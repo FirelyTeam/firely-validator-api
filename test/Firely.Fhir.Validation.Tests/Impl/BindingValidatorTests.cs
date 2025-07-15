ea92b0f13d89c23beccafc281559792fcfc18577
Kasdejong <k.j2.de.jong@student.vu.nl>
2025-07-15 13:07:57 +0200

    fixed some extra null reference warnings

--- test/Firely.Fhir.Validation.Tests/Impl/BindingValidatorTests.cs
+++ test/Firely.Fhir.Validation.Tests/Impl/BindingValidatorTests.cs
@@ -255,7 +255,7 @@ namespace Firely.Fhir.Validation.Tests
 
             static TerminologyServiceExceptionResult userIntervention(ValidateCodeParameters p,
                 FhirOperationException e)
-                => p.Coding?.Code.StartsWith("UNKNOWN") is true
+                => p.Coding?.Code?.StartsWith("UNKNOWN") is true
                     ? TerminologyServiceExceptionResult.Warning
                     : TerminologyServiceExceptionResult.Error;
         }
@@ -280,7 +280,7 @@ namespace Firely.Fhir.Validation.Tests
 
             static TerminologyServiceExceptionResult userIntervention(ValidateCodeParameters p,
                 FhirOperationException e)
-                => p.CodeableConcept?.Coding.Last().Code.EndsWith("error") is true
+                => p.CodeableConcept?.Coding.Last().Code?.EndsWith("error") is true
                     ? TerminologyServiceExceptionResult.Error
                     : TerminologyServiceExceptionResult.Warning;
         }
