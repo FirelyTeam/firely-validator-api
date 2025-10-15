cd2a0279341b4e6ef082cee3ee2d2bf72fac1235
Kasdejong <kas@fire.ly>
2025-10-15 14:31:22 +0200

    effectively merged sdk-6 final version into sdk-5 forward merge so any issues are resolved. Please check the commit, not the full diff.

--- test/Firely.Fhir.Validation.Compilation.Tests.Shared/ValidationIntegrationTests.cs
+++ test/Firely.Fhir.Validation.Compilation.Tests.Shared/ValidationIntegrationTests.cs
@@ -53,7 +53,7 @@ namespace Firely.Fhir.Validation.Tests
         [DynamicData(nameof(getTestSuites), DynamicDataSourceType.Method)]
         public async Task RunValidateTestSuite(string suiteName)
         {
-            var overwrite = false;
+            var overwrite = true;
 
             var pd = new DirectoryInfo(Path.GetFullPath(Path.Combine(getSuiteDirectory(suiteName), "data")));
             var externalReferenceResolver = new FileBasedExternalReferenceResolver(pd);
