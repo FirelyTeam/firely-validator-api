152a7feabdaf7dbba24871563fd8f2c50a75faaf
Rob5045 <rob@robertmark.nl>
2026-02-17 17:26:46 +0100

    Code cleanup.

--- test/Benchmarks/ValidatorBenchmarks.cs
+++ test/Benchmarks/ValidatorBenchmarks.cs
@@ -10,7 +10,6 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
-using Task = System.Threading.Tasks.Task;
 
 //using Validator = Hl7.Fhir.Validation.Validator;
 
