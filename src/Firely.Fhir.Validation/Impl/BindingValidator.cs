6544239329febdcadc2230c35eaf9f470c1f1715
Andrzej Skowroński <andrzej@fire.ly>
2026-03-24 10:22:11 +0100

    Bump version of SDK and package version for release

--- src/Firely.Fhir.Validation/Impl/BindingValidator.cs
+++ src/Firely.Fhir.Validation/Impl/BindingValidator.cs
@@ -252,7 +252,7 @@ namespace Firely.Fhir.Validation
         {
             try
             {
-                var callParams = parameters.Build();
+                var callParams = parameters;
                 return interpretResults(TaskHelper.Await(() => ctx.ValidateCodeService.ValueSetValidateCode(callParams)), display);
             }
             catch (FhirOperationException tse)
